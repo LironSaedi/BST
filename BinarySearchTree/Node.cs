1f0e09f95b649fb74eeac5c974d8a99472470ad5
Liron Saedi <Liron.Saedi@GMR.local>
2020-12-23 18:24:43 -0800

    I started the bst and I am finishing the insert

---
+++ BinarySearchTree/Node.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinarySearchTree
+{
+    class Node<T> where T : IComparable<T>
+    {
+
+        public Node<T> Right { get; set; }
+        public Node<T> Left { get; set; }
+
+        public T Value { get; set; }
+
+        public Node(T Value)
+        {
+            this.Value = Value;
+        }
+
+
+    }
+
+
+}
+
---
+++ BinarySearchTree/Tree.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinarySearchTree
+{
+    class Tree<T> where T : IComparable<T>
+    {
+        Node<T> root;
+
+        public void Insert(T value)
+        {
+            
+                if (root == null)
+                {
+                    Node<T> newNode = new Node<T>(value);
+                    root = newNode;
+                    return;
+                }
+
+                Node<T> node = new Node<T>(value);
+                Node<T> holder = root;
+
+            while (true)
+            {
+
+                int index = node.Value.CompareTo(holder.Value);
+                if (index == 0 || index == 1)
+                {
+                    if (holder.Right == null)
+                    {
+                        //insert to the right and exit the function
+                    }
+                    holder = holder.Right;
+                }
+
+                else
+                {
+                    //go to the left side of the tree
+                }
+
+            }
+        }
+    }
+}
