bcb80356f85ff6137427f6b9bd27ab8c9458b7ca
Liron Saedi <Liron.Saedi@GMR.local>
2021-01-13 18:15:36 -0800

    I am close do being done with the right remove

--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -6,7 +6,21 @@ namespace BinarySearchTree
     {
         static void Main(string[] args)
         {
-            // 5 , 0 , 10
+            
+            Tree<int> tree = new Tree<int>();
+
+            tree.Insert(5);
+            tree.Insert(0);
+            tree.Insert(10);
+            tree.Insert(7);
+            //tree.Insert(11);
+            tree.Delete(10);
+           
+           
+            // TO DO FORNEXT TIME 1/20/21 : FIx the DElete with 1 child. Seems to be deleteing the whole right side. Also test out the delete with 2 children and finsih the left hand side.
+            ;
+
+
 
         }
     }
--- BinarySearchTree/Tree.cs
+++ BinarySearchTree/Tree.cs
@@ -7,21 +7,18 @@ namespace BinarySearchTree
     class Tree<T> where T : IComparable<T>
     {
         Node<T> root;
-
+        bool checker = false;
         public void Insert(T value)
         {
             Node<T> node = new Node<T>(value);
             if (root == null)
             {
-
-
                 root = node;
                 return;
-
             }
 
 
-            root = node;
+
             Node<T> holder = root;
             while (true)
             {
@@ -34,13 +31,13 @@ namespace BinarySearchTree
                         return;
                     }
                     holder = holder.Right;
-                    
+
                 }
 
 
                 else
                 {
-                    if(holder.Left==null)
+                    if (holder.Left == null)
                     {
                         holder.Left = node;
                         return;
@@ -48,8 +45,62 @@ namespace BinarySearchTree
                     holder = holder.Left;
                 }
             }
-        }
 
+
+        }
+        public void Delete(T value)
+        {
+            Node<T> node = new Node<T>(value);
+            if (root == null)
+            {
+                root = node;
+                return;
+            }
+            Node<T> holder = root;
+            int index = node.Value.CompareTo(holder.Value);
+            while (true)
+            {
+                if (index >= 0)
+                {
+                    if (holder.Right.Value.Equals(value))
+                    {
+                        if (holder.Right.Right == null)
+                        {
+                            holder.Right = null;
+                            return;
+                        }
+                        else
+                        {
+                            if (!(holder.Right.Right == null))
+                            {
+                                holder.Right.Right = holder.Right;
+                                return;
+                                // run the 1 node remove
+                            }
+
+                            else
+                            {
+                                holder = holder.Left;
+                                while(holder.Right == null)
+                                {
+                                    holder = holder.Right;
+                                }
+
+                                root.Right = holder;
+                                return;
+                                // run the 2 node or more remove
+                            }
+
+                        }
+                    }
+
+                    else
+                    {
+                        holder = holder.Right;
+                    }
+                }
+            }
+        }
         #region oldcode
         //    if (root == null)
         //    {
